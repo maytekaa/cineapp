51d4c645acdceae771a168fa1c6ad85e32b028d0
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-05 09:33:59 -0300

    page filmes

--- cineapp/src/Componentes/Pages/Filmes.jsx
+++ cineapp/src/Componentes/Pages/Filmes.jsx
@@ -1,12 +1,30 @@
-import data  from "../../../arquivos.json"
-
+import { useState } from "react";
 
 function Filmes(){
-    console.log(data)
+
+  const [filmes, setFilmes] = useState([])
+    
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e')
+    .then(response => response.json())
+    .then(response => setFilmes(response.results))
+    .catch(error => console.log(error))
     return (
       <>
           <h1> Filmes </h1>
-          
+          <div className="listaFilmes">
+          {
+            filmes.map(
+              filme => (
+                <div className="card-filme" key={filme.id}>
+                  <h1>{filme.title}</h1>
+                  <img src={`https://image.tmdb.org/t/p/w342/${filme.poster_path}`}></img>
+                  <p>{filme.overview}</p>
+                </div>
+                
+              )
+            )
+          }
+          </div>
         </>
       );
 }
